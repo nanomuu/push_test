1bf0bb536f03d63fe542b08e4e686ef57a89265b
nanomuu <furaka.082@gmail.com>
2023-03-30 22:55:18 +0900

    課題1.4

---
+++ Hello World/src/sample/selfIntroduction.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class selfIntroduction {
+	public static void main(String[] args){
+		// 氏名
+		String name = "小室春佳";
+		// 年齢
+		int age = 25;
+		// 趣味
+		String hobby = "映画鑑賞";
+		/*
+		 * 自己紹介文を出力する時の処理
+		 * */
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+		}	
+}
