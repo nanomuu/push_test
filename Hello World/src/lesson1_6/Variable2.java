7c7e1c42b8ce19a0f7f91a9b268356d23e5722ec
nanomuu <furaka.082@gmail.com>
2023-04-12 21:42:50 +0900

    課題１.６　再提出

--- Hello World/src/lesson1_6/Variable2.java
+++ Hello World/src/lesson1_6/Variable2.java
@@ -2,11 +2,12 @@ package lesson1_6;
 import java.util.Random;
 public class Variable2 {
 	public static void main(String[] args) {
-//		問１）
+//		＠問１）
 //		変数 x を宣言し、「3」を代入して下さい。
 //		変数 x に自己代入を使って「20」を掛け算して下さい。
 //		その後 x の値をコンソールに表示して下さい。
-        int x = 3;
+        int x;
+        x = 3;
         x = x * 20;
         System.out.println(x);
 //		問２）
