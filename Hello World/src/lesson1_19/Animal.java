69172680050e032e5ce5fc95a6566a0a989bea2b
nanomuu <furaka.082@gmail.com>
2023-06-05 10:52:03 +0900

    1_18 提出

---
+++ Hello World/src/lesson1_19/Animal.java
@@ -0,0 +1,23 @@
+package lesson1_19;
+
+public class Animal {
+	
+	public static void main(String[] args) {
+		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+		class TestClass {
+		    // メンバ変数
+		    public String name = "Dog";
+		    
+		    // メソッド
+		    public String testMethod(){
+		        return "Engineer";
+		    }
+		}
+	
+	
+		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		
+		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+		
+	}
+}
