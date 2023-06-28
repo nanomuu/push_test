76b1a2918486af5defb76170b51efd741f8c8606
nanomuu <furaka.082@gmail.com>
2023-06-29 00:26:55 +0900

    1_19 再提出

--- Hello World/src/lesson1_19/Animal.java
+++ Hello World/src/lesson1_19/Animal.java
@@ -1,41 +1,46 @@
 package lesson1_19;
 
-import java.time.LocalDateTime;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 public class Animal {
-	String name;// インスタンス変数
+	String name = "Dog";// インスタンス変数
 	static int count; // static変数(クラス変数)
+
 	
 	// コンストラクタの定義
-	  Animal() {
-	    count++; // countをインクリメント
-	  }
-	  
+	Animal(){
+	count++; // countをインクリメント
+	}
+		  
 	// インスタンスメソッド(nameを取得するため)
-	  public String getName() {
-	    return name;
-	  }
-	  
+	public String getName() {
+	return name;
+	}
+		  
 	// インスタンスメソッド(nameに値を代入するため)
-	  public void setName(String name) {
-	    this.name = name;
-	  }
-	
+	public void setName(String name) {
+	this.name = name;
+	}
+		
 	// staticメソッド(countを取得するため)
-	  public static int getCount() {
-	    return count;
-	  }
-	
+	public static int getCount() {
+	return count;
+	}
+    
+	 
+	  
 	public static void main(String[] args) {
     // Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
-	// dogという変数名でオブジェクトを作成(インスタンス化)
-	Animal dog = new Animal();
-	dog.setName("Dog"); // dogのnameをセット
-	System.out.println(dog.getName()); // dogのnameを取得
-	
+	Dog dog = new Dog();
+	dog.getName();
 	// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-	System.out.println(Animal.getCount()); // Animalクラスのcountを取得	
+	System.out.println(Dog.count);
 	// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
-	LocalDateTime ldt = LocalDateTime.now();
-	System.out.println(ldt);
-	}
+	//現在時刻の生成、取得
+	Date now = new Date();
+	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	String formatted = dateFormat.format(now); // formatメソッドでフォーマット
+    System.out.println(formatted);
+	}	
 }
+    
--- Hello World/src/lesson1_19/Dog.java
+++ Hello World/src/lesson1_19/Dog.java
@@ -2,27 +2,25 @@ package lesson1_19;
 
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
-	 String name; 
+	public String name; 
 	// Q2：フィールドに動物の数の変数を定義してください。
-	 static int count;
+	static int count;
+	
+	public String getName() {
+	return name;
+	}
+	
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
 	// コンストラクタを定義
-	  Dog(String name) {
-	    this.name = name; // 引数で受け取った値をインスタンス変数に代入している
-	      }
-	  public String getName() {
-		return name;
-		  }
-	  public void setName(String name) {
-		this.name = name;
-		  }
+	  Dog() {
+		count++; // countをインクリメント
+	    String name = "犬"; 
+	    System.out.println(name);      
+	  }
 	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-		class Main {
-		public static void main(String[] args) {
-	// Animal dog = new Animal();
-	    Animal dog = new Animal();
+	  Dog(String name){
+		Dog dog = new Dog();
 	// dog.setName(); 
 		System.out.println(dog.getName());
-   }
-}
+	  }
 }
