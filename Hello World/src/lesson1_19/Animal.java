4c9438ee522c7f4e338046debaa40f1a3421fce2
nanomuu <furaka.082@gmail.com>
2023-06-29 22:48:29 +0900

    1_19 再提出

--- Hello World/src/lesson1_19/Animal.java
+++ Hello World/src/lesson1_19/Animal.java
@@ -2,45 +2,21 @@ package lesson1_19;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-public class Animal {
-	String name = "Dog";// インスタンス変数
-	static int count; // static変数(クラス変数)
 
-	
-	// コンストラクタの定義
-	Animal(){
-	count++; // countをインクリメント
-	}
-		  
-	// インスタンスメソッド(nameを取得するため)
-	public String getName() {
-	return name;
-	}
-		  
-	// インスタンスメソッド(nameに値を代入するため)
-	public void setName(String name) {
-	this.name = name;
-	}
-		
-	// staticメソッド(countを取得するため)
-	public static int getCount() {
-	return count;
-	}
-    
-	 
-	  
+public class Animal {
+
 	public static void main(String[] args) {
-    // Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
-	Dog dog = new Dog();
-	dog.getName();
-	// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-	System.out.println(Dog.count);
-	// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
-	//現在時刻の生成、取得
-	Date now = new Date();
-	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-	String formatted = dateFormat.format(now); // formatメソッドでフォーマット
-    System.out.println(formatted);
-	}	
+		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+		Dog dog = new Dog();
+		System.out.println(dog.name);
+		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		Dog dog1 = new Dog(5);
+		System.out.println(dog1.count);
+		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+		//現在時刻の生成、取得
+		Date now = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String formatted = dateFormat.format(now); // formatメソッドでフォーマット
+		System.out.println(formatted);
+	}
 }
-    
--- Hello World/src/lesson1_19/Dog.java
+++ Hello World/src/lesson1_19/Dog.java
@@ -2,25 +2,18 @@ package lesson1_19;
 
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
-	public String name; 
+	String name;
 	// Q2：フィールドに動物の数の変数を定義してください。
-	static int count;
-	
-	public String getName() {
-	return name;
-	}
-	
+	int count;
+
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
 	// コンストラクタを定義
-	  Dog() {
-		count++; // countをインクリメント
-	    String name = "犬"; 
-	    System.out.println(name);      
-	  }
+	Dog() {
+		this.name = "犬";
+	}
+
 	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-	  Dog(String name){
-		Dog dog = new Dog();
-	// dog.setName(); 
-		System.out.println(dog.getName());
-	  }
+	Dog(int Animalcount) {
+		this.count = Animalcount;
+	}
 }
