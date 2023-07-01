d087e75357bf8400ca062217864eab636d17f84a
nanomuu <furaka.082@gmail.com>
2023-07-01 13:24:56 +0900

    1_19再提出

--- Hello World/src/lesson1_19/Animal.java
+++ Hello World/src/lesson1_19/Animal.java
@@ -7,11 +7,13 @@ public class Animal {
 
 	public static void main(String[] args) {
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+		//Dog.javaからDogクラスを呼び出す
 		Dog dog = new Dog();
 		System.out.println(dog.name);
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		//Dog.javaからDogクラスを呼び出して動物の数を代入
 		Dog dog1 = new Dog(5);
-		System.out.println(dog1.count);
+	    System.out.println(dog1.count);
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 		//現在時刻の生成、取得
 		Date now = new Date();
--- Hello World/src/lesson1_19/Dog.java
+++ Hello World/src/lesson1_19/Dog.java
@@ -7,12 +7,13 @@ public class Dog {
 	int count;
 
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
-	// コンストラクタを定義
+	// 同じ変数名nameを定義
 	Dog() {
 		this.name = "犬";
 	}
 
 	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+	//Q2の変数countに引数Animalcountを代入
 	Dog(int Animalcount) {
 		this.count = Animalcount;
 	}
